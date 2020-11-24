d48daabfb3618a474056e070599ff6d7ff34feac
Mairis K <mairis.krumkalns@inbox.lv>
2020-11-24 20:02:13 +0200

    a

---
+++ src/Comp/Data/post.ts
@@ -0,0 +1,22 @@
+export const postData = [
+    {
+        id: '1',
+        title: 'This is title'
+    },
+    {
+        id: '2',
+        title: 'thhis is title 2'
+    },
+    {
+        id: '3',
+        title: 'This is title 3'
+    },
+    {
+        id: '4',
+        title: 'This is title 4'
+    },
+    {
+        id: '5',
+        title: 'This is title 5'
+    },
+    ]
